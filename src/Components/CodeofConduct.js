70b8b1a7f001dc9074c3bfe497e6aa1fa66c9c71
sumitchoudharySEM <sumitsem2004@gmail.com>
2022-08-05 22:43:12 +0530

    few section updated

--- src/Components/CodeofConduct.js
+++ src/Components/CodeofConduct.js
@@ -2,7 +2,24 @@ import React from 'react'
 
 const CodeofConduct = () => {
   return (
-    <div id='CodeofConduct'>CodeofConduct</div>
+    <div id='CodeofConduct'>
+      <h2>Google Event Code of Conduct</h2>
+      <p>oogle is dedicated to providing a harassment-free and inclusive event experience for everyone regardless of gender identity and expression, sexual orientation, disabilities, neurodiversity, physical appearance, body size, ethnicity, nationality, race, age, religion, or other protected category. We do not tolerate harassment of event participants in any form. Google takes violations of our policy seriously and will respond appropriately.</p>
+      <div className="darkbox">
+        <div className="darkboxdiv box1">
+          <h3>Be excellent to each other</h3>
+          <p>We want the event to be an excellent experience for everyone regardless of gender identity and expression, sexual orientation, disabilities, neurodiversity, physical appearance, body size, ethnicity, nationality, race, age, religion, or other protected category. Treat everyone with respect. Participate while acknowledging that everyone deserves to be here -- and each of us has the right to enjoy our experience without fear of harassment, discrimination, or condescension, whether blatant or via micro-aggressions. Jokes shouldn’t demean others. Consider what you are saying and how it would feel if it were said to or about you.</p>
+        </div>
+        <div className="box2 darkboxdiv ">
+          <h3>Be excellent to each other</h3>
+          <p>We want the event to be an excellent experience for everyone regardless of gender identity and expression, sexual orientation, disabilities, neurodiversity, physical appearance, body size, ethnicity, nationality, race, age, religion,ence without fear of harassment, discrimination, or condescension, whether blatant or via micro-aggressions. Jokes shouldn’t demean others. Consider what you are saying and how it would feel if it were said to or about you.</p>
+        </div>
+        <div className="darkboxdiv box3">
+          <h3>Be excellent to each other</h3>
+          <p>We want the event to be an excellent experience for everyone regardless of gender identity and expression, sexuay, race, age, religion, or other protected category. Treat everyone with respect. Participate while acknowledging that everyone deserves to be here -- and each of us has the right to enjoy </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/Components/Speakers.js
+++ src/Components/Speakers.js
@@ -7,7 +7,13 @@ const Speakers = () => {
       <p>Hear from the Professionals who are building the future of cloud. Our speakers are influential folks allies who have been associated with communities within their organisations, cities, country and beyond.</p>
       <div className="card-grid">
 
-        <div className="speaker-card">  
+        <div className="speaker-card"> 
+        <img src="https://sessionize.com/image/4952-400o400o2-ak93xPqkbF4SbF1uc9yEc6.jpg" alt="" /> 
+        <h4>Sumit Choudhary</h4>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio dicta voluptatem minus eaque. Dignissimos iure</p>
+        <div className="socialmedia">
+          
+        </div>
         </div>
         <div className="speaker-card">  
         </div>
